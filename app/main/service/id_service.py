53be3f6e3cb29f3b776cdd414a35f048e5da0f23
unknown <jonathan.duplaix@etudiant.univ-lr.fr>
2020-12-08 20:49:20 +0100

    Compile mais bug persistance requete (si req KO, toutes les req+ hérite du result de req KO. Commit avant refonte

--- app/main/service/id_service.py
+++ app/main/service/id_service.py
@@ -1,37 +1,40 @@
 import re
 
-check_status = 0
+response = {
+            "status":"not_checked",
+            "request":"none",
+            "result":2
+        }
 
 def check_id(id_to_check):
 
-    check_structure(id_to_check)
-    if check_status == 0:
-        response = {
-            "status":"success",
-            "request":id_to_check,
-            "result":1
-        }
-    else:
-        response = {
-            "status":"failed",
-            "request":id_to_check,
-            "result":0
-        }
+    check_structure(id_to_check) 
+
+
     return response
 
 
 def check_structure(id_to_check):
-    regex = re.compile('^[A-P|Z][0-9]{9}$')
-    test_regex = regex.match(id_to_check)
-    ## Si l'ID ne match pas la REGEX, il sera un objet de type NoneType
-    ##  sur lequel on ne peut pas .group()
-    try:
-        isinstance(test_regex.group(), str) 
-    except Exception as e:
-        print('Structure de l\'identifiant ' + id_to_check + ' invalide:')
-        print(e)
-        global check_status
-        check_status += 1
+    global response
+    if response['result'] > 0:
+        response['request'] = id_to_check
+        regex = re.compile('^[A-P|Z][0-9]{9}$')
+        test_regex = regex.match(id_to_check)
+        ## Si l'ID ne match pas la REGEX, il sera un objet de type NoneType
+        ## sur lequel on ne peut pas .group()
+        try:
+            isinstance(test_regex.group(), str)
+            response['status'] = "success"
+            response['result'] = 1
+        except Exception as e:
+            print('Structure de l\'identifiant ' + id_to_check + ' invalide:')
+            print(e)
+            response['status'] = "failed"
+            response['result'] = 0
+
+#def check_rule_2(id_to_check):
+
+
 
 
 
